628f2f27c3e196b4d3cd2bbeaa8e4420cc9fb0b1
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-16 13:05:18 +0200

    server problems

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express      = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv       = require("dotenv").config();
 const app          = express();
-const port         = process.env.PORT || 3000;
+const port         = process.env.PORT || 80;
 
 console.log('port', port);
 

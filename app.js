7920e30b2cf41a508b19e51d7cbd6517cd5b9b1a
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-16 13:12:34 +0200

    db structure

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/movies", require("./routes/movieRoutes"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/products", require("./routes/productRoutes"));
+app.use("/courses", require("./routes/courseRoutes"));
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => console.log(`Server running on port ${port}`));

a50b8fd62189c4e677ff25054764835688f4746a
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-16 13:49:36 +0200

    server problems

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv  = require("dotenv").config();
 const app     = express();
-const port    = process.env.PORT || 8080;
+const port    = process.env.PORT || 3000;
 
 console.log('port', port);
 
@@ -9,6 +10,6 @@ app.use(express.json());
 app.use("/users",    require("./routes/userRoutes"));
 app.use("/products", require("./routes/productRoutes"));
 app.use("/courses",  require("./routes/courseRoutes"));
-app.use(cors({origin: '*'}));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

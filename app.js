f20a69f43d969e0e5366869f0b04d3775a9618ad
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-16 13:12:32 +0200

    server port dilema

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv  = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use("/users", require("./routes/userRoutes"));

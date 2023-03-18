4ea248becbf62e355e095798579178b416910eb9
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-18 03:00:48 +0200

    fix allow origin issues

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use("/users",    require("./routes/userRoutes"));
 app.use("/products", require("./routes/productRoutes"));
 app.use("/courses",  require("./routes/courseRoutes"));
 app.use(errorHandler);
-app.use(cors({origin: '*'}));
+app.use(cors());
+app.get('/users/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+app.get('/courses/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

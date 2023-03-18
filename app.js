475b3bda81ee93c725a39fa6bb553447927323e7
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-18 02:32:02 +0200

    allow origin *

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors         = require('cors');
 const express      = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv       = require("dotenv").config();
